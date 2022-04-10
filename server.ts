29dadf523ff618ff0709d52aa3058d1af2a2be1b
Zach Neibaur <zneibaur@protonmail.com>
2022-04-10 08:08:31 -0600

    Adding fetch event handler

--- server.ts
+++ server.ts
@@ -1,11 +1,9 @@
 import { Application } from "https://deno.land/x/oak/mod.ts";
 import router from "./routes.ts"; // Import our router
 
-const PORT = 3001;
 const app = new Application();
 
 app.use(router.routes()); // Implement our router
 app.use(router.allowedMethods()); // Allow router HTTP methods
 
-console.log(`Server listening on port ${PORT}`);
-await app.listen({ port: PORT });
+addEventListener("fetch", app.fetchEventHandler())
