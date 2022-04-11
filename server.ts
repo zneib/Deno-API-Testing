8405562642bcdfa28c1a6a0f2661082fd8a0fedd
Zach Neibaur <zneibaur@protonmail.com>
2022-04-11 00:06:49 -0600

    Adding cors

--- server.ts
+++ server.ts
@@ -1,9 +1,10 @@
 import { Application } from "https://deno.land/x/oak/mod.ts";
+import { oakCors } from "https://deno.land/x/cors/mod.ts";
 import router from "./routes.ts"; // Import our router
 
 const app = new Application();
-
+app.use(oakCors());
 app.use(router.routes()); // Implement our router
 app.use(router.allowedMethods()); // Allow router HTTP methods
 
-addEventListener("fetch", app.fetchEventHandler())
+addEventListener("fetch", app.fetchEventHandler());
