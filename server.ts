f6b3fabeccee9048e5caab169daeb52d06392072
Zach Neibaur <zneibaur@protonmail.com>
2022-04-11 00:22:34 -0600

    Removing deprecated fetchEventHandler

--- server.ts
+++ server.ts
@@ -6,5 +6,4 @@ const app = new Application();
 app.use(oakCors());
 app.use(router.routes()); // Implement our router
 app.use(router.allowedMethods()); // Allow router HTTP methods
-
-addEventListener("fetch", app.fetchEventHandler());
+app.listen()
