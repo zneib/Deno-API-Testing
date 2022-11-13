0813d2802a541871fc5c2caa99767ade35a4fd9d
Zach Neibaur <zneibaur@protonmail.com>
2022-11-12 23:29:44 -0700

    Adding new route for ratings

--- routes.ts
+++ routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "https://deno.land/x/oak/mod.ts";
 import { addTodo, getTodos, getTodo, updateTodo, deleteTodo, getIncompleteTodos } from "./controllers/todos.ts"; // Import controller methods
 import { addBook, getBooks, getBook, updateBook, deleteBook } from "./controllers/books.ts"; // Import controller methods
+import { addRating } from "./controllers/ratings.ts"; // Import controller methods
 
 const router = new Router();
 
@@ -8,6 +9,7 @@ const router = new Router();
 router
   .post("/api/todos", addTodo) // Add a todo
   .post("/api/books", addBook) // Add a book
+  .post("/api/ratings", addRating) // Add a rating
   .get("/api/todos", getTodos) // Get all todos
   .get("/api/books", getBooks) // Get all books
   .get("/api/todos/:id", getTodo) // Get one todo
